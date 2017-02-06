27c8b82b7be244e4be1b02a5b992b631c4f9789a
meijun <jun.mei@outlook.com>
2017-02-06 23:17:51 +0800

    AC model parser

---
+++ ac.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"math"
+	"strconv"
+)
+
+type ACNode interface{}
+
+type AC []ACNode
+
+type ACID int
+
+type VarNode struct {
+	Kth   int
+	Value int
+}
+type NumNode float64
+type MulNode []ACID
+type AddNode []ACID
+
+func LoadAC(acFile string) AC {
+	bs, err := ioutil.ReadFile(acFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bss := bytes.Split(bs, []byte("\n"))
+	end := len(bss) - 1
+	for string(bss[end]) != "EOF" {
+		end--
+	}
+	bss = bss[1:end]
+	ac := make(AC, len(bss))
+	for i, ln := range bss {
+		bs := bytes.Split(ln, []byte(" "))
+		switch string(bs[0]) {
+		case "v":
+			ac[i] = VarNode{
+				Kth:   parseInt(string(bs[1])),
+				Value: parseInt(string(bs[2])),
+			}
+		case "n":
+			ac[i] = NumNode(parseFloat(string(bs[1])))
+		case "*":
+			mn := make(MulNode, len(bs)-1)
+			for j, v := range bs[1:] {
+				mn[j] = ACID(parseInt(string(v)))
+			}
+			ac[i] = mn
+		case "+":
+			an := make(AddNode, len(bs)-1)
+			for j, v := range bs[1:] {
+				an[j] = ACID(parseInt(string(v)))
+			}
+			ac[i] = an
+		}
+	}
+	return ac
+}
+
+type Assign [][]float64
+
+// Return log(Pr(x))
+func (ac AC) Pr(assign Assign) float64 {
+	nn := len(ac)
+	val := make([]float64, nn)
+	for i, n := range ac {
+		switch n := n.(type) {
+		case VarNode:
+			val[i] = math.Log(assign[n.Kth][n.Value])
+		case NumNode:
+			val[i] = math.Log(float64(n))
+		case MulNode:
+			mul := 0.0
+			for _, ai := range n {
+				mul += val[ai]
+			}
+			val[i] = mul
+		case AddNode:
+			sum := 0.0
+			for _, ai := range n {
+				sum += math.Exp(val[ai])
+			}
+			val[i] = math.Log(sum)
+		}
+	}
+	return val[len(val)-1]
+}
+
+func parseInt(s string) int {
+	r, e := strconv.ParseInt(s, 0, 0)
+	if e != nil {
+		log.Fatal(e)
+	}
+	return int(r)
+}
+
+func parseFloat(s string) float64 {
+	r, e := strconv.ParseFloat(s, 64)
+	if e != nil {
+		log.Fatal(e)
+	}
+	return r
+}
---
+++ ac_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"math"
+	"reflect"
+	"testing"
+)
+
+func testLoadAC_dfs(cnt []int, ac AC, v ACID, vis []bool) {
+	if vis[v] {
+		return
+	}
+	vis[v] = true
+	switch nd := ac[v].(type) {
+	case VarNode:
+		cnt[0]++
+	case NumNode:
+		cnt[1]++
+	case MulNode:
+		cnt[2]++
+		for _, u := range nd {
+			testLoadAC_dfs(cnt, ac, u, vis)
+		}
+		cnt[4] += len(nd)
+	case AddNode:
+		cnt[3]++
+		for _, u := range nd {
+			testLoadAC_dfs(cnt, ac, u, vis)
+		}
+		cnt[4] += len(nd)
+	}
+}
+
+func TestLoadAC(t *testing.T) {
+	ac := LoadAC("data/nltcs.ac")
+	cnt := []int{0, 0, 0, 0, 0}
+	testLoadAC_dfs(cnt, ac, ACID(len(ac)-1), make([]bool, len(ac)))
+	if !reflect.DeepEqual(cnt, []int{32, 2625, 288155, 100776, 878825}) {
+		t.Fail() // [var, const, times, plus, edges] from `libra fstats -i nltcs.ac`
+	}
+}
+
+func TestAC_Pr(t *testing.T) {
+	ac := LoadAC("data/nltcs.ac")
+	getAssign := func(a []int) Assign {
+		as := make(Assign, 16)
+		for i := range as {
+			as[i] = []float64{0, 0}
+			as[i][a[i]] = 1
+		}
+		return as
+	}
+	p0 := ac.Pr(getAssign([]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}))
+	if math.Abs(p0-(-1.750023)) > 1e-6 {
+		t.Fail() // from `libra acquery -m nltcs.ac -q nltcs0.q`
+	}
+	p1 := ac.Pr(getAssign([]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}))
+	if math.Abs(p1-(-3.519511)) > 1e-6 {
+		t.Fail() // from `libra acquery -m nltcs.ac -q nltcs1.q`
+	}
+	p01 := ac.Pr(getAssign([]int{0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1}))
+	if math.Abs(p01-(-15.535905)) > 1e-6 {
+		t.Fail() // from nltcs01.q
+	}
+	p10 := ac.Pr(getAssign([]int{1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0}))
+	if math.Abs(p10-(-27.309816)) > 1e-6 {
+		t.Fail() // from nltcs10.q
+	}
+}
